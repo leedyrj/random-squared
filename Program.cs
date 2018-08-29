2ab0e4f5ee24ced1afb0c6707f58177f31893f8b
Robert Leedy <leedyrj@gmail.com>
2018-08-29 10:09:18 -0500

    finished challenge

--- Program.cs
+++ Program.cs
@@ -16,18 +16,15 @@ namespace random
             Random rnd = new Random();
             Byte[] b = new Byte[20];
             rnd.NextBytes(b);
-            Console.WriteLine("The Random bytes are: ");
             for (int i = 0; i <= b.GetUpperBound(0); i++)
                 RndNums.Add(rand.Next(51));
-            // Console.Write("{0,8:N0}", rand.Next(51));
 
             List<double> SqrRnd = new List<double>();
 
 
-            double randomnum;
             foreach (int number in RndNums)
             {
-                randomnum = Math.Pow(number, 2);
+                double randomnum = Math.Pow(number, 2);
                 SqrRnd.Add(randomnum);
             }
 
